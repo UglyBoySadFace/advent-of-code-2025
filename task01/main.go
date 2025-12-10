1f054ab69e4f661feb3482708985a6cfce518c0d
Filip Dubják <filip.dubjak@seznam.cz>
2025-12-10 01:25:32 +0100

    day 1 complete

--- task01/main.go
+++ task01/main.go
@@ -12,17 +12,41 @@ func main() {
 		if err != nil {
 			break
 		}
+
+		counter += count / 100
+		count = count % 100
+
+		if meanwhileClicked(dial, dir, count) {
+			counter++
+		}
+
 		switch dir {
 		case 'L':
-			dial = (dial - count) % 100
+			dial = (dial - count + 100) % 100
 		case 'R':
 			dial = (dial + count) % 100
 		}
 
-		if dial == 0 {
-			counter++
-		}
+		fmt.Println("Dial:", dial, "Counter:", counter)
+
 	}
 
 	fmt.Println("Final counter:", counter)
+}
+
+func meanwhileClicked(dial int, dir uint8, count int) bool {
+	if dial == 0 {
+		return false
+	}
+	switch dir {
+	case 'L':
+		if dial-count <= 0 {
+			return true
+		}
+	case 'R':
+		if dial+count >= 100 {
+			return true
+		}
+	}
+	return false
 }
